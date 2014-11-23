ba2114b4035a0c0923bcce94786f9b522a3724c7
lingdian618 <lijian430421@gmail.com>
2014-11-23 21:20:47 +0800

    Just another push

--- source/about/index.md
+++ source/about/index.md
@@ -1,3 +1,8 @@
-title: about
+layout: about
+title: About
 date: 2014-11-22 20:50:07
 ---
+
+# 我是李健
+## 我不是歌手
+### just another 李健
---
+++ source/gallery/.generate.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+PHO_HOME=~/Pictures/public_photos/
+dst_file=~/Documents/github/hexo/source/gallery/index.md
+
+# 图片尺寸： 宽度500px , 高度500px 
+style_sized='imageView2/2/w/500/h/500/q/85|watermark/2/text/bGlqaWFuLm1s/font/5a6L5L2T/fontsize/500/fill/I0VGRUZFRg==/dissolve/100/gravity/SouthEast/dx/10/dy/10'
+# 图片尺寸： 宽度618px 
+style_small='imageView2/2/w/618/q/100|watermark/2/text/bGlqaWFuLm1s/font/5a6L5L2T/fontsize/500/fill/I0VGRUZFRg==/dissolve/100/gravity/SouthEast/dx/10/dy/10'
+# 图片尺寸： 宽度500px 
+style_thumbnail='imageView2/2/w/500/q/85|watermark/2/text/bGlqaWFuLm1s/font/5a6L5L2T/fontsize/500/fill/I0VGRUZFRg==/dissolve/100/gravity/SouthEast/dx/10/dy/10'
+# 图片尺寸： 高度500px
+style_h500='imageView2/2/h/500/q/85|watermark/2/text/bGlqaWFuLm1s/font/5a6L5L2T/fontsize/500/fill/I0VGRUZFRg==/dissolve/100/gravity/SouthEast/dx/10/dy/10'
+
+prefix='http://public-lingdian618.qiniudn.com/images'
+
+printf "layout: gallery\ntitle: Gallery\ndate: 2014-11-23 09:08:08\n---\n\n" > $dst_file
+
+
+for x in `ls $PHO_HOME`;
+do
+	if [ -d $PHO_HOME/$x ];
+	then
+		printf "\n## %s\n\n" $x >> $dst_file
+		cd $PHO_HOME/$x
+		` ls | cat | xargs -I '{}' printf "<a class=\"article-gallery-img fancybox\" href=\"%s/%s/%s?%s\" >\n\t<img src=\"%s/%s/%s?%s\" itemprop=\"image\">\n</a>\n" $prefix $x '{}' $style_small $prefix $x '{}' $style_h500 >> $dst_file `
+	fi
+done
+
