72e5f168909b90929afcbb1f065221a73f2609c3
lingdian618 <lijian430421@gmail.com>
2014-11-24 21:38:49 +0800

    Just another push

--- source/about/index.md
+++ source/about/index.md
@@ -3,6 +3,16 @@ title: About
 date: 2014-11-22 20:50:07
 ---
 
-# 我是李健
-## 我不是歌手
-### just another 李健
+## 简介
+李健，非歌手，学生一枚。各种ID：lijian430421，lingdian618，bystander，beatlj，水手1。
+
+
+## 经历
+- 2013-now：		研究生，自动化研究所(CASIA)
+- 2009-2013：	大学，中南大学自动化系(CSU)
+- 2006-2009：	高中，衡阳市八中（衡八）
+
+## 联系方式
+- 邮箱：		lijian430421@gmail.com， lijian430421@163.com
+- 微博：		http://weibo.com/u/1857370020
+- github：	https://github.com/lingdian618
--- source/gallery/.generate.sh
+++ source/gallery/.generate.sh
@@ -17,13 +17,14 @@ prefix='http://public-lingdian618.qiniudn.com/images'
 printf "layout: gallery\ntitle: Gallery\ndate: 2014-11-23 09:08:08\n---\n\n" > $dst_file
 
 
-for x in `ls $PHO_HOME`;
+for x in ` ls $PHO_HOME -t `;
 do
 	if [ -d $PHO_HOME/$x ];
 	then
-		printf "\n## %s\n\n" $x >> $dst_file
+		printf "\n\n## %s\n\n" $x >> $dst_file
 		cd $PHO_HOME/$x
-		` ls | cat | xargs -I '{}' printf "<a class=\"article-gallery-img fancybox\" href=\"%s/%s/%s?%s\" >\n\t<img src=\"%s/%s/%s?%s\" itemprop=\"image\">\n</a>\n" $prefix $x '{}' $style_small $prefix $x '{}' $style_h500 >> $dst_file `
+#		` ls | cat | xargs -I '{}' printf "<a class=\"article-gallery-img fancybox\" href=\"%s/%s/%s?%s\" >\n\t<img src=\"%s/%s/%s?%s\" itemprop=\"image\">\n</a>\n" $prefix $x '{}' $style_small $prefix $x '{}' $style_h500 >> $dst_file `
+		` ls | cat | xargs -I '{}' printf "<a class=\"article-gallery-img fancybox\" href=\"%s/%s/%s?%s\" ><img src=\"%s/%s/%s?%s\" itemprop=\"image\"></a>" $prefix $x '{}' $style_small $prefix $x '{}' $style_h500 >> $dst_file `
 	fi
 done
 
